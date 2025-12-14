39bb07ecb4f238e266d1640f067e369f7d836609
hleydens <hleydens@ivytech.edu>
2025-12-14 17:19:33 -0600

    Add files via upload

---
+++ module 7 python script.py
@@ -0,0 +1,7 @@
+numberArray = [1, 4, 7, 13, 4, 58, 15, 35, 45, 21, 41, 25, 18, 68, 73]
+
+for number in numberArray:
+    if number % 2 == 0:
+        print(str(number) + " is even")
+    else:
+        print(str(number) + " is odd")

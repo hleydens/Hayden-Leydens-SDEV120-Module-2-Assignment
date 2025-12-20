0bd5aa50224c636f6b6643d8f6d301156ceebd65
hleydens <hleydens@ivytech.edu>
2025-12-19 20:28:06 -0600

    Add files via upload

---
+++ Hayden Leydens - SDEV120 - Module 8 Python Assignment.py
@@ -0,0 +1,12 @@
+def comparison(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+a = 2
+b = 3
+result = comparison(a,b)
+print(str(a) + " is greater than " + str(b) + " is " + str(result))
+a = 10
+b = 6
+print(str(a) + " is greater than " + str(b) + " is " + str(result))
